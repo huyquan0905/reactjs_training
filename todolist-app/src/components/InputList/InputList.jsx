ced3a215bf22810b59be59e09cf614523a061ca4
QuanHH <quanhh@mirabo-global.com>
2025-04-30 20:28:35 +0700

    add feature input task

--- todolist-app/src/components/InputList/InputList.jsx
+++ todolist-app/src/components/InputList/InputList.jsx
@@ -2,14 +2,46 @@ import React, { Component } from "react";
 import "./input.css";
 
 class InputList extends Component {
+  state = {
+    tasks: [],
+    inputValue: ""
+  };
+
+  handleChange = (e) => {
+    this.setState({ inputValue: e.target.value });
+  };
+
+  handleKeyDown = (e) => {
+    const { inputValue, tasks } = this.state;
+    if (e.key === "Enter" && inputValue.trim() !== "") {
+      this.setState({
+        tasks: [...tasks, inputValue.trim()],
+        inputValue: ""
+      });
+    }
+  };
+
   render() {
+    const { tasks, inputValue } = this.state;
+
     return (
       <div>
         <input
           type="text"
           placeholder="What needs to be done?"
           className="todo-input"
+          value={inputValue}
+          onChange={this.handleChange}
+          onKeyDown={this.handleKeyDown}
         />
+        <ul>
+          {tasks.map((task, index) => (
+            <li key={index}>
+              <input type="checkbox" />
+              <span>{task}</span>
+            </li>
+          ))}
+        </ul>
       </div>
     );
   }
